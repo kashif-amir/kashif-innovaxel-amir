2d239fbf1be4b1d6c4d851ac04325d709675af50
kashif-amir <kashifamir174@gmail.com>
2025-07-14 00:15:07 +0500

    add the before action callback for stat method

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
 
-   before_action :set_link, only: [:show, :update, :destroy]
+   before_action :set_link, only: [:show, :update, :destroy, :stats]
 
   def create
     short_code = SecureRandom.alphanumeric(6)
