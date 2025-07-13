842d72901e21700534efbef02630002d176b690d
kashif-amir <kashifamir174@gmail.com>
2025-07-14 00:05:31 +0500

    add the create short url method and private method

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,23 @@
 class LinksController < ApplicationController
+
+  def create
+    short_code = SecureRandom.alphanumeric(6)
+    @link = Link.new(link_params.merge(short_code: short_code, access_count: 0))
+
+    if @link.save
+      render json: @link, status: :created
+    else
+      render json: @link.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def set_link
+    @link = Link.find_by!(short_code: params[:short_code])
+    rescue ActiveRecord::RecordNotFound
+    render json: { error: "Short URL not found" }, status: :not_found
+  end 
+
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,14 +1,2 @@
 class Link < ApplicationRecord
-
-  def create
-    short_code = SecureRandom.alphanumeric(6)
-    @link = Link.new(link_params.merge(short_code: short_code, access_count: 0))
-
-    if @link.save
-      render json: @link, status: :created
-    else
-      render json: @link.errors, status: :unprocessable_entity
-    end
-  end
-
 end
